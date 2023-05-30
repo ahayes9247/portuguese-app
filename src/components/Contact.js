0a56d3f25b6d5faf3b6095204ba2257ffc5bd61a
ahayes9247 <hayes.amanda@outlook.com>
2023-05-30 10:20:41 +0100

    Corrects URL of image on Contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -41,7 +41,7 @@ function Contact() {
                 </article>
 
                 <aside>
-                    <img id="article-image-right" src={require('../img/sites-image.png')}></img>
+                    <img id="article-image-right" src={require('../img/contact-image.png')}></img>
                 </aside>
             </section>
         </main>
