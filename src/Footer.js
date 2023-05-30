9f3336d5ccadc8e379f996844407dac709ad6b5f
ahayes9247 <hayes.amanda@outlook.com>
2023-05-30 13:57:05 +0100

    Corrects the position of the footer logo

--- src/Footer.js
+++ src/Footer.js
@@ -6,45 +6,46 @@ function Footer() {
         <footer>
             <section id="footer-wrapper">
                 <article id="footer-article">
-                <div id="footer-logo-container">
-                        <a href="/">
-                            <img src={FooterLogo} class="footer-logo-image" border="0"></img>
-                            </a>
-                    </div>
-                    <nav id="footer-nav">
-                        <div id="footer-links">
-                        <ul>
-                            <li>
-                                <a href="/MobileApps">
-                                    Apps
+                    <div id="footer-logo-container">
+                            <a href="/">
+                                <img src={FooterLogo} class="footer-logo-image"></img>
                                 </a>
-                            </li>
-
-                            <li>
-                                <a href="/Books">
-                                    Books
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="/Websites">
-                                    Websites
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="/Contact">
-                                    Contact
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    Top
-                                </a>
-                            </li>
-                        </ul>
                         </div>
-                    </nav>
+
+                        <nav id="footer-nav">
+                            <div id="footer-links">
+                            <ul>
+                                <li>
+                                    <a href="/MobileApps">
+                                        Apps
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="/Books">
+                                        Books
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="/Websites">
+                                        Websites
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="/Contact">
+                                        Contact
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Top
+                                    </a>
+                                </li>
+                            </ul>
+                            </div>
+                        </nav>
                 </article>
             </section>
         </footer>
